1e7f46663bf5e24f116f425f9a8b36ffb39aa351
Quang Nguyen <16520998@gm.uit.edu.vn>
2023-07-09 16:56:44 +0700

    Fix button open close menu mobile of header

--- javascript/btn-menu.js
+++ javascript/btn-menu.js
@@ -1,27 +1,33 @@
 $(document).ready(function () {
   ("use strict");
   var header_tag = document.querySelector("header");
-  var header = document.querySelector(".header-container");
+  var header_container = document.querySelector(".header-container");
   var logo = document.querySelector(".logo-wrapper");
   var btn_open_menu = document.getElementById("btn-mobile");
   var btn_close_menu = document.getElementById("btn-close");
+  
   btn_open_menu.onclick = function () {
     toggleMenu();
-    logo.style.display = "none";
+    header_tag.classList.toggle("header-mobile");
+    header_container.classList.remove("header-container-mobile-close");
+    header_container.classList.toggle("header-container-mobile-menu");
+    logo.classList.toggle("logo-wrapper-none");
+
     btn_open_menu.style.display = "none";
-    header.style.cssText = "justify-content: center;";
-    header_tag.style.backgroundColor = "var(--button-color)";
     btn_close_menu.classList.toggle("active");
   };
 
   btn_close_menu.onclick = function () {
     closeMenu();
-    logo.style.display = "flex";
+    header_tag.classList.toggle("header-mobile");
+    header_container.classList.remove("header-container-mobile-menu");
+    header_container.classList.toggle("header-container-mobile-close");
+    logo.classList.remove("logo-wrapper-none");
+
     btn_open_menu.style.display = "flex";
-    header.style.cssText = "justify-content: space-between;";
-    header_tag.style.backgroundColor = "var(--button-normal-color)";
     btn_close_menu.classList.remove("active");
   };
+
   function toggleMenu() {
     var menu = document.querySelector(".menu-list");
     menu.classList.toggle("active");
